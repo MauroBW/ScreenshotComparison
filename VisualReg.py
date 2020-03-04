eaabe7329cd53d21c4d983c3d44716d07ece3e97
MauroBW <maurobotti99@gmail.com>
2020-03-04 20:58:14 -0300

    Last version with test cases and results (Testing alerts)

--- VisualReg.py
+++ VisualReg.py
@@ -1,77 +1,58 @@
 from PIL import Image, ImageDraw
 from selenium import webdriver
+from tkinter.filedialog import askopenfilename
 import os
 import sys
 import time
 
 class ScreenAnalysis:
 
-    # STAGING_URL = 'https://www.reddit.com/'
-    # PRODUCTION_URL = 'https://www.reddit.com/r/AskReddit/'
     driver = None
 
     def __init__(self):
-        #self.set_up()
-        #self.capture_screens()
         self.analyze()
-        #self.clean_up()
-
-    # def set_up(self):
-    #     self.driver = webdriver.Firefox(executable_path="C:\\Users\\mauro\\Desktop\\Drivers/geckodriver.exe")
-
-    #def clean_up(self):
-        #self.driver.close()
-
-    # def capture_screens(self):
-    #     self.screenshot(self.STAGING_URL, 'screen_staging.png')
-    #     time.sleep(5)
-    #     self.screenshot(self.PRODUCTION_URL, 'screen_production.png')
-    #     time.sleep(5)
-
-
-    # def screenshot(self, url, file_name):
-    #     print ("Capturing", url, "screenshot as", file_name, "...")
-    #     self.driver.get(url)
-    #     #self.driver.set_window_size(1024, 768)
-    #     self.driver.save_screenshot(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'screenshots', file_name))
-    #     self.driver.get_screenshot_as_png()
-    #     print ("Done.")
 
     def analyze(self):
-        screenshot_staging = Image.open("screenshots/prueba1.png")
-        screenshot_production = Image.open("screenshots/prueba2.png")
+        actual_screen = askopenfilename()
+        expected_screen = askopenfilename()
+        screenshot_actual = Image.open(actual_screen)
+        screenshot_expected = Image.open(expected_screen)
         columns = 60
         rows = 80
-        screen_width, screen_height = screenshot_staging.size
+        screen_width, screen_height = screenshot_actual.size
 
-        block_width = ((screen_width - 1) // columns) + 1 # this is just a division ceiling
+        print(screen_width, screen_height)
+
+        block_width = ((screen_width - 1) // columns) + 1 
         block_height = ((screen_height - 1) // rows) + 1
 
         for y in range(0, screen_height, block_height+1):
             for x in range(0, screen_width, block_width+1):
-                region_staging = self.process_region(screenshot_staging, x, y, block_width, block_height)
-                region_production = self.process_region(screenshot_production, x, y, block_width, block_height)
+                region_actual = self.process_region(screenshot_actual, x, y, block_width, block_height)
+                region_production = self.process_region(screenshot_expected, x, y, block_width, block_height)
 
-                if region_staging is not None and region_production is not None and region_production != region_staging:
-                    draw = ImageDraw.Draw(screenshot_staging)
+                if region_actual is not None and region_production is not None and region_production != region_actual:
+                    draw = ImageDraw.Draw(screenshot_actual)
                     draw.rectangle((x, y, x+block_width, y+block_height), outline = "green")
+                    
 
-        screenshot_staging.save("result.png")
+        screenshot_actual.save("result2.png")
+        raise Exception ("The screenshots doesn't  match")
 
     def process_region(self, image, x, y, width, height):
         region_total = 0
 
-        # This can be used as the sensitivity factor, the larger it is the less sensitive the comparison
         factor = 50
 
         for coordinateY in range(y, y+height):
             for coordinateX in range(x, x+width):
                 try:
                     pixel = image.getpixel((coordinateX, coordinateY))
+                    
                     region_total += sum(pixel)/4
                 except:
                     return
-
+        #print(region_total/factor)
         return region_total/factor
 
 ScreenAnalysis()

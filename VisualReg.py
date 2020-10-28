3d53c3d763c7ad178cc130d9948251397ed48ef5
Mauro Botti <33839339+MauroBW@users.noreply.github.com>
2020-10-28 17:32:12 -0300

    Cambios y screenshots para hacer tests
    Agregado de rutas a las imagenes
    Creacion de results dinamico utilizando la hora

--- VisualReg.py
+++ VisualReg.py
@@ -1,9 +1,10 @@
 from PIL import Image, ImageDraw
 from selenium import webdriver
 from tkinter.filedialog import askopenfilename
+from datetime import datetime
 import os
 import sys
-import time
+
 
 class ScreenAnalysis:
 
@@ -12,19 +13,32 @@ class ScreenAnalysis:
     def __init__(self):
         self.analyze()
 
+    def getTime(self):
+        now = datetime.now()
+        dt = now.strftime("%H%M%S")
+        
+        return (dt)
+
     def analyze(self):
-        actual_screen = askopenfilename()
-        expected_screen = askopenfilename()
+
+        actual_screen = "Actual\Actual1.PNG"
+
+        expected_screen = "Expected\Expected1.png"
+        
         screenshot_actual = Image.open(actual_screen)
         screenshot_expected = Image.open(expected_screen)
+        
         columns = 60
         rows = 80
         screen_width, screen_height = screenshot_actual.size
 
-        print(screen_width, screen_height)
+        print("Resolution of Actual screenshot:",screen_width, screen_height)
+        print("Resolution of Expected screenshot", screenshot_expected.size)
 
         block_width = ((screen_width - 1) // columns) + 1 
         block_height = ((screen_height - 1) // rows) + 1
+
+        print(block_width, block_height)
 
         for y in range(0, screen_height, block_height+1):
             for x in range(0, screen_width, block_width+1):
@@ -33,16 +47,16 @@ class ScreenAnalysis:
 
                 if region_actual is not None and region_production is not None and region_production != region_actual:
                     draw = ImageDraw.Draw(screenshot_actual)
-                    draw.rectangle((x, y, x+block_width, y+block_height), outline = "green")
+                    draw.rectangle((x, y, x+block_width, y+block_height), outline = "red")
                     
 
-        screenshot_actual.save("result2.png")
+        screenshot_actual.save("result" + self.getTime() + ".png")
         raise Exception ("The screenshots doesn't  match")
 
     def process_region(self, image, x, y, width, height):
         region_total = 0
 
-        factor = 50
+        factor = 500
 
         for coordinateY in range(y, y+height):
             for coordinateX in range(x, x+width):
@@ -52,7 +66,8 @@ class ScreenAnalysis:
                     region_total += sum(pixel)/4
                 except:
                     return
-        #print(region_total/factor)
+        print(region_total/factor)
         return region_total/factor
 
+    
 ScreenAnalysis()
